dcbc5766938e489ac9cc383c298e96af9fcbe0d7
alleyful <alleyful@naver.com>
2020-10-24 14:05:46 +0900

    update movie controller, patch and search

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +7,19 @@ export class MoviesController {
     return 'All Movies';
   }
 
+  @Get('search')
+  search(@Query('year') searchYear: string) {
+    return `search from Year: ${searchYear}`
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `Get One Movies ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'create movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,7 +28,10 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `patch movie ${movieId}`;
+  patch(@Param('id') movieId: string, @Body() patchData) {
+    return {
+      updateMovie: movieId,
+      ...patchData
+    };
   }
 }
